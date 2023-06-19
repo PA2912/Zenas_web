03c185a6cbbf0f6bd4eac3092412060c8597be4e
PA2912 <132337309+PA2912@users.noreply.github.com>
2023-06-19 23:42:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 import snowflake.connector
 import pandas
 streamlit.title('Zena\'s Amazing Athleisure Catalog12')
+streamlit.text("Hello from Snowflake:1)
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
